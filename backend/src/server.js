f3792cb6db7e3dcddfdce2559c8b9ccf73f722b2
m7Aei <mateuspm2015@hotmail.com>
2019-08-06 17:47:02 -0300

    Add configurações do server e mongoDB

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const databaseConfig = require("./config/database");
+const routes = require("./routes");
+
+const server = express();
+
+mongoose.connect(databaseConfig.uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(process.env.PORT || 3333);
