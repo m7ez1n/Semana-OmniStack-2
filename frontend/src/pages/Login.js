0f7ef79d23c7f4b40d86dc527b7824af33cc8098
m7Aei <mateuspm2015@hotmail.com>
2019-08-07 16:29:09 -0300

    Criando as paginas do projeto

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+import logo from "../assets/logo.svg";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(username);
+  }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="Tindev" />;
+        <input
+          placeholder="Digite seu usuário no Github"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
