3a1e0bc639e82695e710fd91731c2203bb3a74e6
m7Aei <mateuspm2015@hotmail.com>
2019-08-07 16:30:24 -0300

    Alterações nos css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import logo from "./assets/logo.svg";
+import Routes from "./routes";
 
 function App() {
-  return <img src={logo} alt="Tindev" />;
+  return <Routes />;
 }
 
 export default App;
